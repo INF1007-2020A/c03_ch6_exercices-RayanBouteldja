824632d46b59fd4ff62af0b52001ac921cece786
RayanBouteldja <70912595+RayanBouteldja@users.noreply.github.com>
2020-10-14 18:10:49 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,32 +3,72 @@
 
 
 def order(values: list = None) -> bool:
-    if values is None:
         # TODO: Demander les valeurs ici
-        pass
-
-    return False
+    list=[]
+    while len(list)<10:
+        values=list.append(int(input('écrivez 10 nombres entiers: ')))
+    ordonne=True
+    for index in range(len(list)-1):
+        if list[index]>list[index+1]:
+            ordonne=False
+            print('Ce n\'est pas ordonné')
+            break
+    if ordonne:
+       print('La liste est ordonné')
+    return None
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: Demander les mots ici
         pass
+    mot1=list(input('mot 1: '))
+    mot2=list(input('mot 2: '))
+    if len(mot1)==len(mot2):
+        for letter in mot1:
+            if letter in mot2:
+               mot2.remove(letter)
+        if len(mot2)==0:
+            print('annagramme')
+        else:
+            print('pas annagramme')
+    else:
+        print('pas annagramme')
 
-    return False
+    return None
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    if len(items)==len(set(items)):
+        res='pas de doublons'
+    else:
+        res='contient des doublons'
+    
+    return res
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    moyenne=[]
+    for name in student_grades:
+       somme_note=0
+       for note in student_grades[name]:
+           somme_note+=note
+       student_grades[name]=somme_note/len(student_grades[name])
+    moyenne.append(student_grades[name])
+    moyenne=sorted(moyenne,reverse=True)
+    for name in student_grades:
+        if student_grades[name]==moyenne[0]:
+           return {name:student_grades[name]}
 
 
 def histogram(sentence: str) -> tuple:
     # TODO: Créer l'histogramme a l'aide d'un dictionnaire
+    histogramme={}
+    sentence_2=set(sentence)
+    for lettre in sentence_2:
+        histogramme[lettre]=sentence.count(lettre)
+
     #       Afficher l'histogramme et les lettres les plus fréquentes
     #       Retourner l'histogramme et le tableau de lettres
 
